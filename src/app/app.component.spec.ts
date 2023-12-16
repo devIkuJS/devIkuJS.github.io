bca10695337b34c32543db3243ab157d845d7847
devIkuJS <brian125865@gmail.com>
2023-12-16 15:43:08 -0500

    feat: refactor name project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blog-dota'`, () => {
+  it(`should have as title 'devIkuJS.github.io'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blog-dota');
+    expect(app.title).toEqual('devIkuJS.github.io');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('blog-dota app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('devIkuJS.github.io app is running!');
   });
 });
